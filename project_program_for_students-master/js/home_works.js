bc7ac11cf6d57abbd77568a5f29b03499ef875ee
Kanybekov <131442142+KanybekovB@users.noreply.github.com>
2023-07-11 15:26:10 +0600

    Add files via upload

---
+++ project_program_for_students-master/js/home_works.js
@@ -0,0 +1,37 @@
+//*GMAIL CHECK*
+
+const gmailCheck = document.querySelector('#gmail_input');
+const gmailButton = document.querySelector('#gmail_button');
+const gmail_result = document.querySelector('#gmail_result');
+const regExp = /\w{150}@gmail.com$/ // *ДОЛЖЕН БЫТЬ НЕ МЕНЬШЕ 5
+
+gmailButton.addEventListener('click', () => {
+    let checkGmail = regExp.test(gmailCheck.value);
+    if(checkGmail === true) {
+        gmail_result.innerHTML = 'account is found';
+        gmail_result.style.color = 'green';
+    }else{
+        gmail_result.innerHTML = 'account is not found';
+        gmail_result.style.color = 'red';
+    }
+});
+
+
+//*BLOCK*
+
+const childBlock = document.querySelector('.child_block');
+let leftPx = 0; 
+let maxCountPx = 447 // ОГРАНИЧЕНИЕ ПИКСЕЛЕЙ
+let delay = 5 // Задержка в милисекундах
+const moveBlock = () => {
+  leftPx++;
+  console.log(leftPx);
+
+  if (leftPx < maxCountPx) {
+    setTimeout(moveBlock, delay); //* setTimeout метод для замедления
+  }
+
+  childBlock.style.left = `${leftPx}px`;
+};
+
+moveBlock();
